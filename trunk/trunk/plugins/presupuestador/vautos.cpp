86e24c95c7edf80d00c7b2a8d742339d78e36f02
tranfuga25s <tranfuga25s@6109f7bc-ab41-0410-9af0-57f1f6311122>
2009-03-04 01:34:23 +0000

    agregado modelos y delegates al plugin de presupuestador

---
+++ trunk/trunk/plugins/presupuestador/vautos.cpp
@@ -0,0 +1,95 @@
+/***************************************************************************
+ *   Copyright (C) 2007 by Esteban Zeller   *
+ *   juiraze@yahoo.com.ar   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+#include "vautos.h"
+#include <QIcon>
+#include "mauto.h"
+#include <QTableView>
+#include <QSqlRelationalDelegate>
+#include "edpatente.h"
+#include "edmarca.h"
+#include "edmodelo.h"
+#include "edcolor.h"
+
+VAutos::VAutos(QWidget *parent)
+ : EVLista(parent)
+{
+ setObjectName( "VAutos" );
+ setWindowTitle( "Automoviles" );
+ setWindowIcon( QIcon( ":/imagenes/auto.png" ) );
+
+ modelo = new MAuto( this );
+
+ vista->setModel( modelo );
+ vista->hideColumn( 6 );
+ vista->hideColumn( 7 );
+ vista->setItemDelegateForColumn( 1, new QSqlRelationalDelegate( vista ) );
+ vista->setItemDelegateForColumn( 0, new EDPatente( vista ) );
+ vista->setItemDelegateForColumn( 2, new EDMarca( vista ) );
+ vista->setItemDelegateForColumn( 3, new EDModelo( vista ) );
+ vista->setItemDelegateForColumn( 5, new EDColor( vista ) );
+
+ modelo->select();
+
+ addAction( ActAgregar );
+ addAction( ActEliminar );
+ addAction( ActCerrar );
+}
+
+
+VAutos::~VAutos()
+{}
+
+
+
+#include <QSqlRecord>
+/*!
+    \fn VAutos::antes_de_insertar( int row, QSqlRecord & record )
+ */
+void VAutos::antes_de_insertar( int row, QSqlRecord & record )
+{
+}
+
+
+/*!
+    \fn VAutos::agregar()
+ */
+void VAutos::agregar()
+{
+ EVLista::agregar( false );
+}
+
+
+/*!
+    \fn VAutos::contextMenuEvent ( QContextMenuEvent * e )
+ */
+void VAutos::contextMenuEvent ( QContextMenuEvent * e )
+{
+ QModelIndex indice = vista->indexAt( e->pos() );
+ if( indice.isValid() )
+ {
+   //Creo el menu
+   qWarning( "Crear menu" );
+   EVLista::contextMenuEvent( e );
+ }
+ else
+ {
+  e->ignore();
+ }
+}
