b9749ecea25f11f3c6e92c76bbe9d3f462a7b9ec
tranfuga25s <tranfuga25s@6109f7bc-ab41-0410-9af0-57f1f6311122>
2011-06-27 18:19:32 +0000

    Arregladas tablas sqlite de servicios y otro

--- trunk/plugins/ventas/ventas.QSQLITE.sql
+++ trunk/plugins/ventas/ventas.QSQLITE.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS factura ( "id_factura" INTEGER PRIMARY KEY autoincrement, "id_cliente" INTEGER NOT NULL, "fecha" TEXT NOT NULL, "id_forma_pago" INTEGER NOT NULL, "serie" INTEGER NOT NULL, "numero" INTEGER NOT NULL );
+CREATE TABLE IF NOT EXISTS factura ( "id_factura" INTEGER PRIMARY KEY autoincrement, "id_cliente" INTEGER NOT NULL, "fecha" TEXT NOT NULL, "id_forma_pago" INTEGER NOT NULL, "serie" INTEGER NOT NULL, "numero" INTEGER NOT NULL, "total" NUMERIC NOT NULL, "id_ctate" NUMERIC NULL, "id_mov_caja" NUMERIC NULL );
 CREATE TABLE IF NOT EXISTS item_factura ( "id_item_factura" INTEGER PRIMARY KEY autoincrement, "id_factura" INTEGER NOT NULL, "cantidad" DOUBLE NOT NULL, "texto" TEXT NOT NULL, "precio_unitario" DOUBLE NOT NULL, "id_producto" INTEGER NULL );
 INSERT OR IGNORE INTO sqlite_sequence VALUES ( 'factura', '1');
 INSERT OR IGNORE INTO sqlite_sequence VALUES ( 'item_factura', '1' );
