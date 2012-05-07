cf58872502afca8a8629db6a0d250352117ef765
tranfuga25s <tranfuga25s@6109f7bc-ab41-0410-9af0-57f1f6311122>
2012-05-07 21:22:55 +0000

    - Arreglado problema de parametro al seleccionar los datos de los descuentos aplicados a un presupuesto. - Agregada la capcidad de ver los descuentos de un presupuesto. - Agregada capacidad de ver los descuentos aplicados a una factura. - Al agregar una compra se actualiza el listado.

--- trunk/plugins/compras/vcompras.cpp
+++ trunk/plugins/compras/vcompras.cpp
@@ -72,7 +72,7 @@ VCompras::VCompras(QWidget *parent)
 void VCompras::agregar( bool /*autoeliminarid*/ )
 {
     FormAgregarCompra *f = new FormAgregarCompra( qobject_cast<MCompra *>( this->rmodelo ) );
-    connect( f, SIGNAL( actualizarVista() ), vista, SLOT( update() ) );
+    connect( f, SIGNAL( actualizarVista() ), this, SLOT( mostrarTodos() ) );
     emit agregarVentana( f );
 }
 
