feb1c73d2975144aabf14e63f0c067685c0adfc7
tranfuga25s <tranfuga25s@gmail.com>
2012-10-30 14:18:37 +0000

    Arreglado problema de impresión.

--- trunk/utiles/EReporte.cpp
+++ trunk/utiles/EReporte.cpp
@@ -97,9 +97,13 @@ bool EReporte::hacer( ParameterList parametros, bool previsualizar, bool mostrar
     _rep->setParamList( _parametros );
 
     if( _impresora == 0 ) {
-        _impresora = new QPrinter( QPrinterInfo::defaultPrinter() );
+        if( !QPrinterInfo::defaultPrinter().isNull() ) {
+            _impresora = new QPrinter( QPrinterInfo::defaultPrinter() );
+        } else {
+            _impresora = new QPrinter();
+        }
     }
-    if( !( _rep->print( _impresora, mostrarDialogoImpresion, previsualizar ) ) ) {
+    if( !( _rep->print( 0, mostrarDialogoImpresion, previsualizar ) ) ) {
         qDebug( "Error al intentar imprimir el reporte o se cancelo" );
         _rep->reportError( 0 );
         return false;
