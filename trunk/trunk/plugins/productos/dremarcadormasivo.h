c8cedd78cff9c4bcd843cbdf18d07b614af99de3
tranfuga25s@gmail.com <tranfuga25s@gmail.com@6109f7bc-ab41-0410-9af0-57f1f6311122>
2013-04-05 16:08:17 +0000

    - Agregado modelo para el remarcador masivo y dialogo.

---
+++ trunk/trunk/plugins/productos/dremarcadormasivo.h
@@ -0,0 +1,29 @@
+#ifndef DREMARCADORMASIVO_H
+#define DREMARCADORMASIVO_H
+
+#include "ui_dremarcadormasivobase.h"
+#include "mtempproductoremarcar.h"
+
+class DRemarcadorMasivo : public QDialog, private Ui::DRemarcadorMasivo
+{
+  Q_OBJECT
+  
+public:
+  DRemarcadorMasivo( QWidget *parent = 0 );
+  
+protected:
+  void changeEvent(QEvent *e);
+
+protected slots:
+  void cambioAPorcentaje();
+  void cambioAMontoFijo();
+  void agregarProducto();
+  void eliminarProducto();
+  void accept();
+
+private:
+  MTempProductoRemarcar *modelo;
+
+};
+
+#endif // DREMARCADORMASIVO_H
