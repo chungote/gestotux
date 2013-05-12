bb5c8eb8c2e5b190d50dd33d666ca279f1667375
tranfuga25s <tranfuga25s@6109f7bc-ab41-0410-9af0-57f1f6311122>
2013-05-12 14:43:07 +0000

    - Agregada clase utilitaria para el modelo del equipamiento.

---
+++ trunk/trunk/plugins/ordentrabajo/ecbmodeloequipamiento.h
@@ -0,0 +1,14 @@
+#ifndef ECBMODELOEQUIPAMIENTO_H
+#define ECBMODELOEQUIPAMIENTO_H
+
+#include "ecbtabla.h"
+
+class ECBModeloEquipamiento : public ECBTabla
+{
+    Q_OBJECT
+public:
+    ECBModeloEquipamiento( QWidget *parent = 0 );
+    
+};
+
+#endif // ECBMODELOEQUIPAMIENTO_H
