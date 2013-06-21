1b9debc7470d48bb336d3bff3b88f96612694069
tranfuga25s <tranfuga25s@6109f7bc-ab41-0410-9af0-57f1f6311122>
2013-06-21 13:41:04 +0000

    - Agregado formulario de rango de fechas y agregado parametro en el reporte.

--- trunk/plugins/informes/gananciaventa/formrangofechas.cpp
+++ trunk/plugins/informes/gananciaventa/formrangofechas.cpp
@@ -1,5 +1,10 @@
 #include "formrangofechas.h"
 
+#include <QDate>
+#include <QSqlQuery>
+#include <QSqlError>
+#include <QSqlRecord>
+
 FormRangoFechas::FormRangoFechas( QWidget *parent ) :
 QDialog(parent)
 {
@@ -7,7 +12,20 @@ QDialog(parent)
     this->setWindowTitle( "Elija el rango" );
 
     // Busco los maximos y mínimos
-    /// @TODO Buscar maximos y minimos de fechas posibles
+    QSqlQuery cola;
+    if( cola.exec( "SELECT MAX(fecha), MIN(fecha) FROM factura" ) ) {
+        if( cola.next() ) {
+            DEDesde->setMinimumDate( cola.record().value(1).toDate() );
+            DEHasta->setMinimumDate( DEDesde->date().addDays( 1 ) );
+            DEHasta->setMaximumDate( cola.record().value(0).toDate() );
+            DEDesde->setMaximumDate( DEHasta->date().addDays( -1 ) );
+        }
+    } else {
+        qWarning( "No se pudieron encontrar datos de las fechas maximas y minimas permitidas. No se controlará" );
+        qDebug( "Error de ejecución de la cola de obtención de fecha maxima y minima de reporte de ventas y ganancias" );
+        qDebug( cola.lastError().text().toLocal8Bit() );
+        qDebug( cola.lastQuery().toLocal8Bit() );
+    }
 }
 
 void FormRangoFechas::accept()
