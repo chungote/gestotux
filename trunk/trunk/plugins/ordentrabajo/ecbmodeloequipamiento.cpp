611027d0c5a00abd0e2aa0631428eb779766893e
tranfuga25s@gmail.com <tranfuga25s@gmail.com@6109f7bc-ab41-0410-9af0-57f1f6311122>
2013-06-28 14:29:11 +0000

    - Mejorado formulario de equipamientos.

--- trunk/trunk/plugins/ordentrabajo/ecbmodeloequipamiento.cpp
+++ trunk/trunk/plugins/ordentrabajo/ecbmodeloequipamiento.cpp
@@ -6,6 +6,6 @@ ECBTabla(parent)
     this->setObjectName("CBModelosEquipamiento");
     this->setearTabla( "equipamiento" );
     this->setearCampoId( "DISTINCT modelo" );
-    this->setearCampoTexto( "DISTINCT modelo" );
-    this->setearCampoOrden( "DISTINCT modelo" );
+    this->setearCampoTexto( "modelo" );
+    this->setearCampoOrden( "modelo" );
 }
--- trunk/trunk/plugins/ordentrabajo/formequipamiento.cpp
+++ trunk/trunk/plugins/ordentrabajo/formequipamiento.cpp
@@ -10,10 +10,7 @@ FormEquipamiento::FormEquipamiento( QWidget *parent ) :
   this->setWindowTitle( "Equipamiento" );
   this->setWindowIcon( QIcon(":/imagenes/equipamiento.png" ) );
 
-  /// @TODO Inicializar cb de marcas
   /// @TODO ver modelo de historial
-  /// @TODO Hacer autocompletado de marcas
-
   _id = -1;
   _editar = false;
   _mequipamiento = 0;
@@ -27,9 +24,25 @@ FormEquipamiento::FormEquipamiento( QWidget *parent ) :
  * Setea el modelo de equipamiento para su uso.
  * \param m Puntero al modelo a utilizar
  */
-void FormEquipamiento::setearModeloEquipamiento(MEquipamiento *m)
+void FormEquipamiento::setearModeloEquipamiento( MEquipamiento *m )
 {
   if( m != 0 ) { _mequipamiento = m; }
+}
+
+/*!
+ * \brief FormEquipamiento::setearAgregar
+ * Setea el estado del formulario de edición a agregado de nuevo equipamiento
+ * \param estado si desea colocarse en agregar o no
+ */
+void FormEquipamiento::setearAgregar( bool estado )
+{
+  if( estado ) {
+    GBHistorial->setVisible( false );
+    _editar = false;
+  } else {
+    GBHistorial->setVisible( true );
+    _editar = true;
+  }
 }
 
 /*!
