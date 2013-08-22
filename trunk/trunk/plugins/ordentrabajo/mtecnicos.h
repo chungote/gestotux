fd3cf3815b7d0b25589b0e31171f9558ba26b7c1
tranfuga25s <tranfuga25s@6109f7bc-ab41-0410-9af0-57f1f6311122>
2013-08-22 12:21:44 +0000

    - Mejorada compilación de trsistemas. - Agregada salida personalizada para la vista de tecnicos. - Mejorado menu contextual de tecnicos.

--- trunk/trunk/plugins/ordentrabajo/mtecnicos.h
+++ trunk/trunk/plugins/ordentrabajo/mtecnicos.h
@@ -8,6 +8,8 @@ class MTecnicos : public QSqlTableModel
     Q_OBJECT
 public:
     MTecnicos( QObject *parent = 0 );
+    QVariant data(const QModelIndex &idx, int role) const;
+    Qt::ItemFlags flags(const QModelIndex &index) const;
 
     static bool existe( const QString nombre );
     bool tieneDatosRelacionados( const int id_tecnico );
