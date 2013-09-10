6863262962a3981aa1dac7f2d0651e3f11b519b7
tranfuga25s <tranfuga25s@6109f7bc-ab41-0410-9af0-57f1f6311122>
2013-09-10 23:00:43 +0000

    Mejorado tipos de historial de orden de trabajo. Agregadas acciones.

--- trunk/trunk/plugins/ordentrabajo/mtipooperacionordentrabajo.h
+++ trunk/trunk/plugins/ordentrabajo/mtipooperacionordentrabajo.h
@@ -10,9 +10,13 @@ class MTipoOperacionOrdenTrabajo : public QSqlTableModel
 public:
 
     enum TipoOperacionOrdenTrabajo {
-        CambioTecnico = 1,
-        CambioCliente = 2,
-        CambioEquipamiento = 3
+        CambioTecnico = 1, /** Indica que la orden cambio de tecnico responsable */
+        CambioCliente = 2, /**  Indica que la orden cambio de cliente */
+        CambioEquipamiento = 3, /** Indica que la orden sufrió el cambio de equipamiento */
+        CierreOrden = 4, /** Indica que la orden fue cerrada */
+        DevolucionCliente = 5, /** Indica que la orden fue terminada por devolucion al cliente */
+        Cancelacion = 6, /** Indica que la orden fue cancelada */
+        Facturacion = 7 /** Indica que el evento es una facturacion */
     };
 
     MTipoOperacionOrdenTrabajo(QObject *parent = 0);
