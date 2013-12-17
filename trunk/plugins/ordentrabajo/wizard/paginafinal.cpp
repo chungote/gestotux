f5fb530e7591af0428c180e9bb3fd2051a691795
tranfuga25s <tranfuga25s@6109f7bc-ab41-0410-9af0-57f1f6311122>
2013-12-17 18:06:30 +0000

    Modificaciones para poder guardar una nueva orden de trabajo.

--- trunk/plugins/ordentrabajo/wizard/paginafinal.cpp
+++ trunk/plugins/ordentrabajo/wizard/paginafinal.cpp
@@ -10,12 +10,13 @@ PaginaFinal::PaginaFinal(QWidget *parent) :
     setTitle( "Final: Datos de la orden" );
     setSubTitle( QString::fromUtf8( "Por favor, ingrese los datos necesarios para la orden de trabajo." ) );
 
-    registerField( "orden_trabajo.requerente"    , LERequerente  ); // Persona que ingresa el pedido si no es el cliente.
-    registerField( "orden_trabajo.ingresante*"   , LEIngresante  ); // Ingresante de la orden ( empleado )
-    registerField( "orden_trabajo.fecha_ingreso*", DEIngreso     ); // Fecha de ingreso de la orden
-    registerField( "orden_trabajo.fecha_entrega" , DTEDevolucion ); // Fecha posible de devolucion de la orden
-    registerField( "orden_trabajo.causa_ingreso" , PTEFallas     ); // Causa o falla del ingreso
-    registerField( "orden_trabajo.id_tecnico*"   , CBTecnico     , "idActual" ); // Tecnico asignado
+    registerField( "orden_trabajo.requerente"       , LERequerente  ); // Persona que ingresa el pedido si no es el cliente.
+    registerField( "orden_trabajo.ingresante*"      , LEIngresante  ); // Ingresante de la orden ( empleado )
+    registerField( "orden_trabajo.fecha_ingreso*"   , DEIngreso     ); // Fecha de ingreso de la orden
+    registerField( "orden_trabajo.fecha_entrega"    , DTEDevolucion ); // Fecha posible de devolucion de la orden
+    registerField( "orden_trabajo.causa_ingreso"    , PTEFallas     , "plainText" ); // Causa o falla del ingreso
+    registerField( "orden_trabajo.id_tecnico"       , CBTecnico     , "idActual"  ); // Tecnico asignado
+    registerField( "orden_trabajo.fecha_vencimiento", DEVencimiento ); // Fecha de vencimiento
 }
 
 int PaginaFinal::nextId() const
@@ -30,7 +31,7 @@ void PaginaFinal::initializePage()
     p->beginGroup( "Preferencias" );
     p->beginGroup( "OrdenTrabajo" );
     DTEDevolucion->setDate( QDate::currentDate().addDays( p->value("dias",2 ).toInt() ) );
-    CBTecnico->setearId( p->value( "id_tecnico", 0 ).toInt() );
+    CBTecnico->setearId( p->value( "id_tecnico", 1 ).toInt() );
     p->endGroup();
     p->endGroup();
     p=0;
